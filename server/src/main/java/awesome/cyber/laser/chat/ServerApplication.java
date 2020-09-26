e489c5eee0ae7206a1ee787a8f26c16df9cd459a
Florian Klein <florian.klein@homevoice.io>
2020-09-26 14:18:38 +0200

    backend: add basic backend project configuration

---
+++ server/src/main/java/awesome/cyber/laser/chat/ServerApplication.java
@@ -0,0 +1,11 @@
+package awesome.cyber.laser.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
