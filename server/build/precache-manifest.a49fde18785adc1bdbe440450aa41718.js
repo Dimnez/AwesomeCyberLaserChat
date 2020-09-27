4b392ff6c073830e7ba5b9876e73997ca3622622
Daniel Unknown <marner.daniel@googlemail.com>
2020-09-27 16:45:06 +0200

    added react build folder

---
+++ server/build/precache-manifest.a49fde18785adc1bdbe440450aa41718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11b7322614f9b572445a5d8d6483eef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b843e1b67d248883ab",
+    "url": "/static/css/main.28385f9a.chunk.css"
+  },
+  {
+    "revision": "57c4233350c62a574a12",
+    "url": "/static/js/2.41847903.chunk.js"
+  },
+  {
+    "revision": "05033123f322321a67e5e3d4818e72b1",
+    "url": "/static/js/2.41847903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b843e1b67d248883ab",
+    "url": "/static/js/main.9e3c615a.chunk.js"
+  },
+  {
+    "revision": "1427e1b65357edc00cdf",
+    "url": "/static/js/runtime-main.8e985885.js"
+  }
+]);
