689e66e17f41c45bd8012832816415d12dc9ce69
Daniel Unknown <marner.daniel@googlemail.com>
2020-09-27 16:39:07 +0200

    created a start script for the main folder (server + client); moved the build folder into the server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var Pusher = require('pusher');
+const path = require('path');
 
 var pusher = new Pusher({
     appId: '1080185',
@@ -11,10 +12,16 @@ var pusher = new Pusher({
     encrypted: true
   });
   
+
+console.log("server is running");
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+
+app.use(express.static(path.resolve('build/')));   
+
 
 app.post('/pusher/auth', function(req, res) {
   var socketId = req.body.socket_id;
@@ -30,5 +37,7 @@ app.post('/pusher/send', function(req, res) {
 });
   
 var port = process.env.PORT || 5000;
+
+
 app.listen(port);
 
